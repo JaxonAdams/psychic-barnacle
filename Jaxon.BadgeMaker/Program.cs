64ed1813ccccd46aa982d946aa27c74b9a8f7d53
Jaxon Adams <jadamsresume1@gmail.com>
2022-07-15 11:52:51 -0600

    project init

---
+++ Jaxon.BadgeMaker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jaxon.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string> ()
+            {
+                "Jaxon",
+                "Paige"
+            };
+
+            employees.Add("Brea");
+            employees.Add("Graham");
+
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            };
+        }
+    }
+}
